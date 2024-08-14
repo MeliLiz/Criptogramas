60449d80d58ca301d3a2fb73e5d0d194d8ed5d8c
MeliLiz <melifernandez@ciencias.unam.mx>
2024-08-13 20:29:03 -0600

    with frequencie

--- Criptograma.py
+++ Criptograma.py
@@ -11,36 +11,53 @@ def count_letters(text):
     return letters
 
 # Function to sort the letters by the number of times they appear
-def sort_letters(letters):
+def sort_letters(text):
+    counts = count_letters(text)
     def get_count(letter):
-        return letter[1]
-    return sorted(letters.items(), key=get_count, reverse=True)
+        return counts[letter]
+    return sorted(counts.keys(), key=get_count, reverse=True)
 
+# Function to get the sustitutions of the letters in the criptograma in a dictionary
 def get_sustitutions(criptograma, freq_ordered_letters):
-    letters = sort_letters(count_letters(criptograma))
-    sustitutions = {}
+    letters = sort_letters(criptograma) # Letters in the criptograma ordered by the number of times they appear
+    sustitution = {}
     for i in range(len(letters)):
-        sustitutions[letters[i]] = freq_ordered_letters[i]
-    return sustitutions
+        sustitution[letters[i]] = freq_ordered_letters[i]
+    return sustitution
 
 # Function to sustitute the letters in the text
-# sustitutions is a dictionary with the letters to be sustituted
-def sustitute_text(text, sustitutions):
+# Order_freq_letters is a list with the letters ordered by the number of times they tend to appear in a text
+def sustitute_text(text, order_freq_letters):
+    sustitutions = get_sustitutions(text, order_freq_letters)
     new_text = []
     for letter in text:
         if letter.isalpha():
-            new_text.append(sustitutions[letter[0]])
+            new_text.append(sustitutions[letter])
         else:
             new_text.append(letter)
     return "".join(new_text)
 
+def caesar_cipher(text, n):
+    new_text = []
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for i in range(len(text)):
+        if text[i].isalpha():
+            index = alphabet.index(text[i])
+            new_index = (index + n) % 26
+            new_text.append(alphabet[new_index])
+        else:
+            new_text.append(text[i])
+            
+    return "".join(new_text)
+    
+
 if __name__ == "__main__":
+    
     with open("criptograma_1.txt", "r") as file:
         criptograma = file.read()
-        
-    freq_ordered_letters = ['e', 'a', 'o', 's', 'r', 'n', 'i', 'd', 'l', 't', 'u', 'c', 'm', 'p', 'v', 'g', 'b', 'q', 'f', 'h', 'j', 'z', 'x', 'y', 'k', 'w']
-    sustitutions = get_sustitutions(criptograma.lower(), freq_ordered_letters)
-    print(sustitutions)
-    print(sustitute_text(criptograma, sustitutions))
     
-        
+    """freq_ordered_letters = ['e', 'a', 'o', 's', 'r', 'n', 'i', 'd', 'l', 'c', 't', 'u', 'm', 'p', 'b', 'g', 'y', 'v', 'q', 'h', 'f', 'z', 'j', 'x', 'k', 'w']
+    
+    print(sustitute_text(criptograma.lower(), freq_ordered_letters))"""
+    
+    caesar_cipher()
